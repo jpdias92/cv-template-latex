aea540f49cd026b73ed4a6c4105d0de5b1d51727
João Dias <joaodias@Joao-Dias-MacBook-Pro.local>
2020-12-28 20:30:29 +0000

    Update compile script with relative paths and gitignore

--- compile.sh
+++ compile.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+
+# Constants
 IMAGE=jpdias92/cv-template-latex:1.00
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" latexmk -pdflatex=lualatex -cd -f -interaction=batchmode -pdf template.tex
+CV_DIR=cvs/template-cv
+CV_TEX_FILE=template-cv.tex
+
+cd $(dirname $0)
+
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" bash -c \
+"ls; pwd;
+cp twentysecondcv.cls $CV_DIR;
+latexmk -pdflatex=lualatex -cd -f -interaction=batchmode -pdf $CV_DIR/$CV_TEX_FILE
+rm $CV_DIR/twentysecondcv.cls"
