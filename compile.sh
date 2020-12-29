326626e0ebae5e59c17937c2d51ec6ea24b81871
João Dias <joaodias@Joao-Dias-MacBook-Pro.local>
2020-12-29 13:55:32 +0000

    Fix missing fonts

--- compile.sh
+++ compile.sh
@@ -9,6 +9,6 @@ cd $(dirname $0)
 
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" bash -c \
 "ls; pwd;
-cp twentysecondcv.cls $CV_DIR;
+cp twentysecondcv.cls $CV_DIR; cp -R fonts/ $CV_DIR/
 latexmk -pdflatex=lualatex -cd -f -interaction=batchmode -pdf $CV_DIR/$CV_TEX_FILE
-rm $CV_DIR/twentysecondcv.cls"
+rm $CV_DIR/twentysecondcv.cls; rm -rf $CV_DIR/fonts"
